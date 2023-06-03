151249927796dc163ca21f00e24202cfade412ca
1SJulioS1 <91168844+1SJulioS1@users.noreply.github.com>
2023-06-02 22:40:59 -0500

    add security

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,5 +1,15 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import generics
+from rest_framework.viewsets import ModelViewSet
+from rest_framework import permissions
+
 from django.shortcuts import render
-# from .models import Menu
+from django.contrib.auth.models import User
+
+from .models import Menu, Booking
+from .serializers import UserSerializer, BookingSerializer, MenuSerializer
 # Create your views here.
 
 
@@ -10,3 +20,24 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html', {})
+
+
+class BookingViewSet(ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class UserViewSet(ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
