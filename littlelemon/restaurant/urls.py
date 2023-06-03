e69c9caa22165ecfee2e72d1fc7429f97d8a3c38
1SJulioS1 <91168844+1SJulioS1@users.noreply.github.com>
2023-06-02 22:04:56 -0500

    add views to api

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from . import views
+from .views import MenuItemsView, SingleMenuItemView
 urlpatterns = [
-    path('', views.index, name='index')
+    # path('', views.index, name='index'),
 
-    #    path("", views.home, name="home"),
-    #    path("about/", views.about, name="about"),
-    #    path("book/", views.book, name="book"),
-    #    path("menu/", views.menu, name="menu"),
+    path("menu/", MenuItemsView.as_view(), name="menu"),
+    path("menu/<int:pk>", SingleMenuItemView.as_view(), name="menu"),
+    # path("booking/", BookingView.as_view(), name="menu"),
     #    path("menu_item/<int:pk>", views.display_menu_item, name="menu_item"),
 ]
